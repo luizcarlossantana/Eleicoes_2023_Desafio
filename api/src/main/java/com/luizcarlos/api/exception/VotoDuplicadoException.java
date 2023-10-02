b974efcc1779c2ccd500e1cf72bb54caba43fff5
Luiz Carlos Santana <112254950+luizcarlossantana@users.noreply.github.com>
2023-10-01 21:15:02 -0300

    criando Exception personalizada com HandleException

---
+++ api/src/main/java/com/luizcarlos/api/exception/VotoDuplicadoException.java
@@ -0,0 +1,8 @@
+package com.luizcarlos.api.exception;
+
+public class VotoDuplicadoException extends RuntimeException{
+
+    public VotoDuplicadoException(String message) {
+        super(message);
+    }
+}
